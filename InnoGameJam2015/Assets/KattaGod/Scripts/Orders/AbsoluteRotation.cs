63532658c37864ed18234641b3ad44e7a48a99c8
Christian Oeing <christian.oeing@slashgames.org>
2015-11-08 14:14:28 +0100

    CHANGED: Orders - Absolute rotation for icons in bubbles.

---
+++ InnoGameJam2015/Assets/KattaGod/Scripts/Orders/AbsoluteRotation.cs
@@ -0,0 +1,28 @@
+﻿namespace KattaGod.Orders
+{
+    using UnityEngine;
+
+    public class AbsoluteRotation : MonoBehaviour
+    {
+        #region Fields
+
+        public Vector3 RotationEuler;
+
+        #endregion
+
+        #region Methods
+
+        protected void Start()
+        {
+            this.UpdateRotation();
+        }
+
+        [ContextMenu("Update Rotation")]
+        private void UpdateRotation()
+        {
+            this.transform.rotation = Quaternion.Euler(this.RotationEuler);
+        }
+
+        #endregion
+    }
+}
