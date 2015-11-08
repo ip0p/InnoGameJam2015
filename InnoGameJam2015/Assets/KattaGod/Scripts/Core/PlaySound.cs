da4f5b74fc98fb7a3f457ac8f931ca81d61004b7
Christian Oeing <christian.oeing@slashgames.org>
2015-11-08 13:39:25 +0100

    ADDED: Fire - Added burning and frying sounds when fire is burning.

---
+++ InnoGameJam2015/Assets/KattaGod/Scripts/Core/PlaySound.cs
@@ -0,0 +1,51 @@
+﻿namespace KattaGod.Core
+{
+    using Slash.Unity.DataBind.Foundation.Setters;
+
+    using UnityEngine;
+
+    public class PlaySound : ComponentSingleSetter<MonoBehaviour, bool>
+    {
+        #region Fields
+
+        public AudioClip Clip;
+
+        public AudioSource Source;
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnValueChanged(bool newValue)
+        {
+            if (newValue)
+            {
+                this.Play();
+            }
+            else
+            {
+                this.Stop();
+            }
+        }
+
+        private void Play()
+        {
+            if (this.Source != null && this.Clip != null)
+            {
+                this.Source.clip = this.Clip;
+                this.Source.loop = true;
+                this.Source.Play();
+            }
+        }
+
+        private void Stop()
+        {
+            if (this.Source != null)
+            {
+                this.Source.Stop();
+            }
+        }
+
+        #endregion
+    }
+}
